bae78ddf4d2253a97563c04902aecca841e0b683
Caio Stigliani Recamon <caio.stig@gmail.com>
2020-07-12 01:48:16 -0300

    Refatorando código e usando async await

--- Routes/users.js
+++ Routes/users.js
@@ -3,50 +3,54 @@ const router = express.Router();
 const Users = require('../model/user');
 const bcrypt = require('bcrypt');
 
-router.get('/', (req, res) => {
-    Users.find({}, (err, data) => {
-        if (err) return res.send({ error: 'Erro na consulta de usuários' });
-        return res.send(data);
-    });
+
+router.get('/', async (req, res) => {
+    try {
+        const users = await Users.find({});
+        return res.send(users);
+    }
+    catch (err) {
+        return res.send({ error: 'Erro na consulta de usuários' });
+    }
 });
 
-router.post('/create', (req, res) => {
+router.post('/create', async (req, res) => {
     const { email, password } = req.body;
-
     if(!email || !password) return res.send({ error: 'Dados insuficientes' }); //tentar criar usuário sem e-mail/senha
 
-    Users.findOne({email}, (err, data) => {
-        if (err) return res.send({ error: 'Erro ao buscar usuário' }); //valida erro na busca do email
-        if (data) return res.send({ error: 'Usuário já registrado' }); //valida se o e-mail ja existe
+    try {
+        if (await Users.findOne({ email })) return res.send({ error: 'Usuário já foi registrado '}); //valida se o e-mail ja existe
 
-        Users.create(req.body, (err, data) => {
-            if (err) return res.send({ error: 'Erro ao criar usuário' }); //Valida erro na criação do usuário
-
-            data.password = undefined;
-            return res.send(data); //se passou por todos os if, o usuário é criado
-        });
-    });
+        const user = await Users.create(req.body);
+        user.password = undefined;
+        return res.send(user);
+    }
+    catch (err) {
+        return res.send( {error: 'Erro ao buscar usuário' }); //valida erro na busca do email
+    }
 });
 
-//endpoint de autenticação
-router.post('/auth', (req, res) => {
+    //endpoint de autenticação
+router.post('/auth', async (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password) return res.send({ error: 'Dados insuficientes!' });
 
-    Users.findOne({email}, (err, data) => {
-        if (err) return res.send({ error: 'Erro ao buscar usuário' });
-        if (!data) return res.send({ error: 'Usuário não registrado' });
+    try {
+        const user = await Users.findOne({ email }).select('+password');
+        if (!user) return res.send({ error: 'Usuário não registrado' });
 
         //vai criptografar a senha que o usuário envia, e comparar se é a mesma que foi criptografada do banco
-        bcrypt.compare(password, data.password, (err, same) => {
-            if (!same) return res.send({ error: 'Erro ao autenticar usuário' });
- 
-            data.password = undefined //não mostrar a senha criptografada no body do postman
-            return res.send(data);
-        })
-    }).select('+password') //está obrigando a mostrar o password para compação
+        const pass_ok = await bcrypt.compare(password, user.password);
 
+        if (!pass_ok) return res.send({ error: 'Erro ao autenticar usuário' });
+
+        user.password = undefined; //não mostrar a senha criptografada no body do postman
+        return res.send(user);
+    }
+    catch (err) {
+        return res.send({ error: 'Erro ao buscar usuário' });
+    }
 });
 
 module.exports = router;
